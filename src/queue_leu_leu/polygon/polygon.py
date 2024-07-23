ca690e9054b059841f9da62baa907dbe2210bf38
xorblo-doitus <blabla86420@gmail.com>
2024-07-23 07:28:51 +0200

    Remove unnecessary TODO

---
+++ src/queue_leu_leu/polygon/polygon.py
@@ -0,0 +1,129 @@
+# You can use any other library that includes standard Vector things
+from pygame import Vector2
+from math import pi, cos, sin, tan, radians
+
+
+def Vector2_polar(magnitude: float, angle_rad: float) -> Vector2:
+  return magnitude * Vector2(cos(angle_rad), sin(angle_rad))
+
+
+class Polygon:
+  """
+  Warning: Make sure to call `bake()` each time `points` are modified.
+  Warning: Consecutive points can't be at the same position (zero-lenght sides are forbidden)
+  """
+  
+  def __init__(self, points: list[Vector2] = []) -> None:
+    self.points: list[Vector2] = points
+    
+    self._vectors: list[Vector2] = []
+    self._growth_vectors: list[Vector2] = []
+    
+    self.bake()
+  
+  @property
+  def vectors(self) -> list[Vector2]:
+    """Read-only: The vector at index i is the translation from point i to point i+1"""
+    return self._vectors
+  
+  def bake(self) -> None:
+    """
+    This method should be called each time points are modified.
+    You can do modifications in bulk without calling this method between each operation,
+    but make sure to call this method at the end of the modifications in order to update internal caches.
+    """
+    if len(self.points) >= 2:
+      self.points = [point for point, next_ in zip(self.points, self.points[1:] + [self.points[0]]) if point != next_]
+    self._bake_vectors()
+    self._bake_growth_vectors()
+  
+  def _bake_vectors(self) -> None:
+    if len(self.points) >= 2:
+      self._vectors = [end - start for start, end in zip(self.points, self.points[1:] + [self.points[0]])]
+    else:
+      self._vectors = []
+  
+  def _bake_growth_vectors(self) -> None:
+    if len(self.points) < 3:
+      self._growth_vectors = []
+      return
+    
+    new_growth_vectors: list[Vector2] = []
+    
+    for before, after in zip([self._vectors[-1]] + self._vectors, self._vectors):
+      direction: Vector2 = (before.normalize() + after.normalize()).normalize().rotate(-90)
+      new_growth_vectors.append(
+        direction / sin(radians(direction.angle_to(after)))
+      )
+      # new_growth_vectors.append((before.normalize() + after.normalize()).normalize().rotate(-90) / 2 * tan(radians(before.normalize().angle_to(after.normalize()))))
+    
+    self._growth_vectors = new_growth_vectors
+  
+  def growed(self, distance: float) -> "Polygon":
+    return Polygon(list(map(lambda point, growth_vector: point + growth_vector * distance, self.points, self._growth_vectors)))
+  
+  def project(self, point: Vector2, segment_i: int) -> Vector2:
+    return (point - self.points[segment_i]).project(self._vectors[segment_i]) + self.points[segment_i]
+  
+  def get_segment_progress(self, point: Vector2, segment_i: int) -> float:
+    """Assumes point lies on the segment"""
+    relative_to_start: Vector2 = point - self.points[segment_i]
+    return relative_to_start.x / self._vectors[segment_i].x if self._vectors[segment_i].x else relative_to_start.y / self._vectors[segment_i].y
+
+class PolygonFollower:
+  def __init__(self, pos: Vector2, size: float):
+    self.pos = pos
+    self.size = size
+
+
+class PolygonFollow:
+  def __init__(self, spacing: float, gap: float, polygon: Polygon, leader: PolygonFollower):
+    """
+    :param spacing: distance between followers
+    :param gap: minimum distance between rings
+    :param max_angle: max angle each side, at back of the leader
+    :param leader: the leader
+    """
+    self.leader: PolygonFollower = leader
+    self.followers: list[PolygonFollower] = []
+    self.polygons: list[Polygon] = []
+    self.spacing: float = spacing
+    self.gap: float = gap
+    self.polygon: Polygon = polygon
+    self.rotation: float = 0
+
+  @property
+  def rotation_deg(self) -> float:
+    return self.rotation / pi * 180
+  
+  @rotation_deg.setter
+  def rotation_deg(self, new: float):
+    self.rotation = new / 180 * pi
+
+  def update_pos(self, new_pos: Vector2):
+    """Update the position of the leader"""
+    self.adapt() # TODO remove this once the change checking is done
+    
+    self.leader.pos = new_pos
+    
+    # Update followers
+    pass
+  
+  def adapt(self):
+    """
+    Update polygons and follower placement
+    """
+    pass
+  
+
+  def add_follower(self, follower: PolygonFollower):
+    """Add a new follower"""
+    self.followers.append(follower)
+
+  def pop_follower(self, index: int=-1):
+    self.followers.pop(index)
+
+  def remove_follower(self, follower: PolygonFollower):
+    """Remove a follower of the trail"""
+    self.pop_follower(self.followers.index(follower))
+  
